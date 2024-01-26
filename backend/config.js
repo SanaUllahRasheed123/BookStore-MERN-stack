59a8ec475698fd27710ee98c9d0df22fd9a25fe4
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-26 21:11:44 +0500

    upload

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://sanaullahrasheed3:340ZbHwUqYlHstaA@books-store-mern.hoec4zf.mongodb.net/books-collection?retryWrites=true&w=majority'
